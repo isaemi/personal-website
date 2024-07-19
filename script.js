0394859a422bf9f04656ad15b1620c1ae61e59b3
isaemi <imsam0124@gmail.com>
2024-07-18 23:54:21 -0700

    07/18/2024

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ let text = document.getElementById('text');
 let mountain1 = document.getElementById('mountain1');
 let mountain2 = document.getElementById('mountain2');
 let mountain3 = document.getElementById('mountain3');
-let sun = document.getElementByIdById('sun');
+let sun = document.getElementById('sun');
+let stars = document.getElementById('stars');
 
 window.addEventListener('scroll', () =>{
     let value = window.scrollY;
-
     text.style.marginTop = value * 2.5 + 'px';
     mountain1.style.left = value * -1.5 + 'px';
     mountain2.style.left = value * 1.5 + 'px';
     mountain3.style.top = value * 1.5 + 'px';
     sun.style.top = value * 1.5 + 'px';
+    stars.style.top = value * -1.5 + 'px';
 });
